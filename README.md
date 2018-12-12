0636c3171e1b2405fb1096f4ba49aaa6fda57543
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2018-12-12 21:43:18 +0100

    test_popen.c, README.md, gitignore

---
+++ README.md
@@ -0,0 +1,17 @@
+# test_popen.c
+
+* [macOS](#macOS)
+* [GNU-Linux](#gnu-linux)
+
+## macOS
+
+```
+$ ./a.out
+-n Hello World!
+```
+## GNU-Linux
+
+```
+$ ./a.out
+Hello World!
+```
---
+++ test_popen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (){
+	const char * command = "echo -n \"Hello World!\"";
+	FILE * stream = popen(command, "r");
+
+	if (!stream) {
+		printf("error: popen(%s) returned %p\n", command, stream);
+	}
+	else{
+		char buffer[1024+1] = {0};
+		while (!feof(stream)){
+			if (fgets(buffer, 1024, stream) != NULL){
+				printf("%s", buffer);
+			}
+		}
+		pclose(stream);
+		stream = NULL;
+	}
+	return 0;
+}
